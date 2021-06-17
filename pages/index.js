8d8a6676c1db0e1bf6d5ac134f4537e40c8a0d54
Saef Taher <saef.taher@dkbcodefactory.com>
2021-06-17 21:20:15 +0200

    Sticky img

--- pages/index.js
+++ pages/index.js
@@ -6,26 +6,26 @@ function Home() {
 }
 
 import pdfHelper from '../pdfHelper'
-export async function getServerSideProps({req, res, query}) {
-  const exportPDF = query.exportPDF === "true";
-  const isServer = !!req;
+// export async function getServerSideProps({req, res, query}) {
+//   const exportPDF = query.exportPDF === "true";
+//   const isServer = !!req;
 
-  if (isServer && exportPDF) {
-    const buffer = await pdfHelper.componentToPDFBuffer(
-      <div>1</div>
-    );
+//   if (isServer && exportPDF) {
+//     const buffer = await pdfHelper.componentToPDFBuffer(
+//       <div>1</div>
+//     );
 
-    // with this header, your browser will prompt you to download the file
-    // without this header, your browse will open the pdf directly
-    res.setHeader("Content-disposition", 'attachment; filename="export.pdf');
+//     // with this header, your browser will prompt you to download the file
+//     // without this header, your browse will open the pdf directly
+//     res.setHeader("Content-disposition", 'attachment; filename="export.pdf');
 
-    // set content type
-    res.setHeader("Content-Type", "application/pdf");
+//     // set content type
+//     res.setHeader("Content-Type", "application/pdf");
 
-    // output the pdf buffer. once res.end is triggered, it won't trigger the render method
-    res.end(buffer);
-  }
-	return { props: {} }
-}
+//     // output the pdf buffer. once res.end is triggered, it won't trigger the render method
+//     res.end(buffer);
+//   }
+// 	return { props: {} }
+// }
 
 export default Home;
